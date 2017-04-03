8abfc7eb4b7a2a18ad92f1b5d4f012990f7f4fa0
Tyler Hampton <dr.frankinfurter@gmail.com>
2017-04-03 03:25:17 -0700

    master: more metadata and added stove

--- metadata.rb
+++ metadata.rb
@@ -6,14 +6,5 @@ description 'Installs/Configures packer_chef_integration'
 long_description 'Installs/Configures packer_chef_integration'
 version '0.1.0'
 
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
-# uploaded to a Supermarket.
-#
-# issues_url 'https://github.com/<insert_org_here>/packer_chef_integration/issues' if respond_to?(:issues_url)
-
-# The `source_url` points to the development reposiory for this cookbook.  A
-# `View Source` link will be displayed on this cookbook's page when uploaded to
-# a Supermarket.
-#
-# source_url 'https://github.com/<insert_org_here>/packer_chef_integration' if respond_to?(:source_url)
+issues_url 'https://github.com/howdoicomputer/packer_chef_integration/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/howdoicomputer/packer_chef_integration' if respond_to?(:source_url)
