24f9ff12a3024b0be683508ffb3a704d5fa868c9
Tyler Hampton <dr.frankinfurter@gmail.com>
2017-04-03 08:07:58 -0700

    master: remove instance id for now since it breaks what chef client expects as a node name

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'howdoicomputer@fastmail.com'
 license 'apachev2'
 description 'Installs/Configures packer_chef_integration'
 long_description 'Installs/Configures packer_chef_integration'
-version '0.1.4'
+version '0.1.5'
 
 issues_url 'https://github.com/howdoicomputer/packer_chef_integration/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/howdoicomputer/packer_chef_integration' if respond_to?(:source_url)
